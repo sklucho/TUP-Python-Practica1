ff2df6f8b50d1ac797ed12990ffbcf4e4f4e08fb
sklucho <perg911@gmail.com>
2022-08-08 17:08:25 -0300

    practica 1,2,3

--- exercises/exercise4.py
+++ exercises/exercise4.py
@@ -11,7 +11,7 @@ numero_03 = "789"
 numero_04 = "132"
 
 # COMPLETAR - INICIO
-
+suma_de_numeros = int(numero_01) + int(numero_02) + int(numero_03) + int(numero_04)
 # COMPLETAR - FIN
 
 assert suma_de_numeros == 1500
@@ -26,7 +26,7 @@ numero_02 = 456
 numero_03 = 789
 
 # COMPLETAR - INICIO
-
+suma_de_numeros_string = str(numero_01) + str(numero_02) + str(numero_03)
 # COMPLETAR - FIN
 
 assert suma_de_numeros_string == "123456789"
